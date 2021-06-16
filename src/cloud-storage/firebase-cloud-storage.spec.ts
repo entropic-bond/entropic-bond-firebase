22cea319088ce068b0086b3a1c4ed0a6c336fb73
Josep Seto <jsetojseto@gmail.com>
2021-06-17 00:54:35 +0200

    tests on StoredFile streaming

--- src/cloud-storage/firebase-cloud-storage.spec.ts
+++ src/cloud-storage/firebase-cloud-storage.spec.ts
@@ -1,22 +1,52 @@
-(global as any).XMLHttpRequest = require('xhr2')
+/**
+ * @jest-environment node
+ */
+ (global as any).XMLHttpRequest = require('xhr2')
 import fetch from 'node-fetch'
-import { CloudStorage, StoredFile } from 'entropic-bond'
 import { FirebaseCloudStorage } from './firebase-cloud-storage'
 import { FirebaseHelper } from '../firebase-helper'
+import { FirebaseDatasource } from '../store/firebase-datasource'
+import { CloudStorage, Model, Persistent, persistent, registerClassFactory, Store, StoredFile } from 'entropic-bond'
 
 // Note about tests leaking. I've been checking and looks like firebase.storage 
 // methods are the responsible for the test leaking (as firebase v. 8.6.3).
 
+class File {	
+	data: Uint8Array
+	name: string
+	lastModified: any
+	size: number
+	type: any
+	arrayBuffer: any
+	slice: any
+	stream: any
+	text: any
+}
+global.File = File
+
+@registerClassFactory( 'Test', ()=>new Test() )
+class Test extends Persistent {
+
+	get file(): StoredFile {
+		return this._file
+	}
+	
+	@persistent private _file: StoredFile = new StoredFile()
+}
+
 describe( 'Firebase Cloud Storage', ()=>{
-	const fileData = new Uint8Array([0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x2c, 0x20, 0x77, 0x6f, 0x72, 0x6c, 0x64, 0x21]);
-	const fileData2 = new Uint8Array([0x6c, 0x6c, 0x6f, 0x2c, 0x48, 0x65, 0x20, 0x77, 0x6f, 0x72, 0x6c, 0x64, 0x21]);
+	const blobData1 = new Uint8Array([0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x2c, 0x20, 0x77, 0x6f, 0x72, 0x6c, 0x64, 0x21]);
+	const blobData2 = new Uint8Array([0x6c, 0x6c, 0x6f, 0x2c, 0x48, 0x65, 0x20, 0x77, 0x6f, 0x72, 0x6c, 0x64, 0x21]);
 	let file: StoredFile
 
 	FirebaseHelper.setFirebaseConfig({
 		projectId: 'demo-test',
 		storageBucket: 'default-bucket'
 	})
-	FirebaseHelper.useEmulator()
+	FirebaseHelper.useEmulator({ firestorePort: 9080 })
+	Store.useDataSource( new FirebaseDatasource() )
+
+
 	
 	beforeEach(()=>{
 		CloudStorage.useCloudStorage( new FirebaseCloudStorage() )
@@ -24,32 +54,32 @@ describe( 'Firebase Cloud Storage', ()=>{
 	})
 
 	it( 'should save and get a url', async ()=>{
-		await file.store( fileData )
+		await file.store( blobData1 )
 
 		expect( file.url ).toContain( file.id )
 	})
 	
 	it( 'should report metadata', async ()=>{
-		await file.store( fileData, 'test.dat' )
+		await file.store( blobData1, 'test.dat' )
 
 		expect( file.originalFileName ).toEqual( 'test.dat' )
 		expect( file.provider.className ).toEqual( 'FirebaseCloudStorage' )
 	})
 
 	it( 'should delete file', async ()=>{
-		await file.store( fileData )
+		await file.store( blobData1 )
 
 		await file.delete()
 		expect( file.url ).not.toBeDefined()		
 	})
 
 	it( 'should overwrite file on subsequent writes', async ()=>{
-		await file.store( fileData )
+		await file.store( blobData1 )
 		const firstUrl = file.url
 		let resp = await fetch( file.url )
 		expect( await resp.text() ).toEqual( 'Hello, world!')
 
-		await file.store( fileData2 )
+		await file.store( blobData2 )
 		resp = await fetch( file.url )
 		expect( 
 			file.url.slice( 0, file.url.indexOf('token') ) 
@@ -62,11 +92,51 @@ describe( 'Firebase Cloud Storage', ()=>{
 	it( 'should trigger events', done=>{
 		const cb = jest.fn()
 
-		file.store( fileData ).then( ()=>{
+		file.store( blobData1 ).then( ()=>{
 			expect( cb ).toHaveBeenCalledTimes( 2 )
 			done()
 		})
 		
 		file.uploadControl().onProgress( cb )
 	})
+
+	describe( 'Streaming', ()=>{
+		let model: Model<Test>
+		let testObj: Test
+
+		beforeEach(()=>{
+			testObj = new Test()
+			model = Store.getModel<Test>( testObj )
+		})
+
+		it( 'should load object with StoredFile', async ()=>{
+			await testObj.file.store( blobData1, 'test.dat' )
+			await model.save( testObj )
+
+			const newTestObj = await model.findById( testObj.id )
+
+			expect( newTestObj.file ).toBeInstanceOf( StoredFile )
+			expect( newTestObj.file.url ).toContain( testObj.file.id )
+		})
+
+		it( 'should replace file on save after load', async ()=>{
+			const deleteSpy = jest.spyOn( testObj.file, 'delete' )
+
+			await testObj.file.store( blobData1, 'test.dat' )
+			await model.save( testObj )
+
+			const newTestObj = await model.findById( testObj.id )
+
+			expect( newTestObj.file ).toBeInstanceOf( StoredFile )
+			expect( newTestObj.file.url ).toContain( testObj.file.id )
+			expect( deleteSpy ).not.toHaveBeenCalled()
+
+			testObj.file.setDataToStore( blobData2 )
+			await testObj.file.store()
+
+			expect( deleteSpy ).toHaveBeenCalled()
+		})		
+
+	})
+
 })
