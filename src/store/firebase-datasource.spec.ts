2734382f5c064ec81d62889bd954a12bb17b5c20
Josep Seto <jsetojseto@gmail.com>
2021-06-17 01:03:54 +0200

    more tests for references

--- src/store/firebase-datasource.spec.ts
+++ src/store/firebase-datasource.spec.ts
@@ -1,22 +1,32 @@
 import fetch from 'node-fetch'
-import { Model, Store } from 'entropic-bond'
+import { Model, Persistent, Store } from 'entropic-bond'
 import { FirebaseDatasource } from './firebase-datasource'
 import { FirebaseHelper } from '../firebase-helper'
 import { TestUser, DerivedUser, SubClass } from '../mocks/test-user'
 import mockData from '../mocks/mock-data.json'
 
-describe( 'Model', ()=>{
-	let model: Model< TestUser >
+async function loadTestData( model: Model<TestUser> ) {
+	const users = mockData.TestUser
+	const promises = []
+	for ( const key in users ) {
+		const user = Persistent.createInstance<TestUser>( users[ key ] )
+		promises.push( model.save( user ) )
+	}
+	await Promise.all( promises )
+}
+
+describe( 'Firestore Model', ()=>{
+	let model: Model<TestUser>
 	let testUser: TestUser
 
 	FirebaseHelper.setFirebaseConfig({
 		projectId: "demo-test",
 	})
 	FirebaseHelper.useEmulator({ firestorePort: 9080 })
-	
-	beforeEach( async ()=> {
+
+	beforeEach( async ()=>{
 		Store.useDataSource( new FirebaseDatasource() )
-		
+
 		testUser = new TestUser()
 		testUser.name = {
 			firstName: 'testUserFirstName',
@@ -24,20 +34,14 @@ describe( 'Model', ()=>{
 		}
 		testUser.age = 35
 		testUser.skills = [ 'lazy', 'dirty' ]
-		
+
 		model = Store.getModel<TestUser>( 'TestUser' )
 
-		const users = mockData.TestUser
-
-		for( const key in users ) {
-			const user = new TestUser()
-			user.fromObject( users[ key ] as any )
-			await model.save( user )
-		}
+		await loadTestData( model )
 	})
 
 	afterEach( async ()=>{
-		FirebaseHelper.instance.firestore().terminate() 
+		FirebaseHelper.instance.firestore().terminate()
 		await fetch( 'http://localhost:9080/emulator/v1/projects/demo-test/databases/(default)/documents', {
 			method: 'DELETE'
 		})
@@ -45,7 +49,7 @@ describe( 'Model', ()=>{
 
 	it( 'should find document by id', async ()=>{
 		await model.save( testUser )
-		
+
 		const user = await model.findById( testUser.id )
 
 		expect( user ).toBeInstanceOf( TestUser )
@@ -57,15 +61,15 @@ describe( 'Model', ()=>{
 		await model.save( testUser )
 		const newUser = await model.findById( testUser.id )
 
-		expect( newUser.name ).toEqual({ 
+		expect( newUser.name ).toEqual({
 			firstName: 'testUserFirstName',
 			lastName: 'testUserLastName'
 		})
-	})	
-	
+	})
+
 	it( 'should delete a document by id', async ()=>{
 		await model.save( testUser )
-		
+
 		const newUser = await model.findById( testUser.id )
 		expect( newUser.age ).toBe( 35 )
 
@@ -82,7 +86,7 @@ describe( 'Model', ()=>{
 				.catch( done )
 		}).not.toThrow()
 	})
-	
+
 	it( 'should return undefined if a document id doesn\'t exists', async ()=>{
 		expect( await model.findById( 'nonExistingId' ) ).toBeUndefined()
 	})
@@ -92,7 +96,7 @@ describe( 'Model', ()=>{
 		const newUser = await model.findById( testUser.id )
 
 		expect( Array.isArray( newUser.skills ) ).toBeTruthy()
-		expect( newUser.skills ).toEqual( expect.arrayContaining([ 'lazy', 'dirty' ]))
+		expect( newUser.skills ).toEqual( expect.arrayContaining([ 'lazy', 'dirty' ]) )
 	})
 
 	it( 'should retrieve object fields', async ()=>{
@@ -120,14 +124,14 @@ describe( 'Model', ()=>{
 			})
 
 			expect( admins.length ).toBeGreaterThanOrEqual( 1 )
-			expect( admins[0] ).toBeInstanceOf( TestUser )
+			expect( admins[ 0 ] ).toBeInstanceOf( TestUser )
 		})
 
 		it( 'should find all admins with where methods', async ()=>{
 			const admins = await model.find().where( 'admin', '==', true ).get()
 
 			expect( admins.length ).toBeGreaterThanOrEqual( 1 )
-			expect( admins[0] ).toBeInstanceOf( TestUser )
+			expect( admins[ 0 ] ).toBeInstanceOf( TestUser )
 		})
 
 		it( 'should find admins with age less than 56', async ()=>{
@@ -137,10 +141,10 @@ describe( 'Model', ()=>{
 				.get()
 
 			expect( admins.length ).toBeGreaterThanOrEqual( 1 )
-			expect( admins[0].age ).toBeLessThan( 50 )
+			expect( admins[ 0 ].age ).toBeLessThan( 50 )
 		})
 
-		
+
 	})
 
 	describe( 'Derived classes should fit on parent collection', ()=>{
@@ -164,11 +168,16 @@ describe( 'Model', ()=>{
 
 		beforeEach( async ()=>{
 			testUser.documentRef = new SubClass()
-			testUser.documentRef.year = 2045	
+			testUser.documentRef.year = 2045
 			ref1 = new SubClass(); ref1.year = 2081
 			ref2 = new SubClass(); ref2.year = 2082
 			testUser.manyRefs.push( ref1 )
 			testUser.manyRefs.push( ref2 )
+			testUser.derived = new DerivedUser()
+			testUser.derived.salary = 1350
+			testUser.manyDerived = [ new DerivedUser(), new DerivedUser() ]
+			testUser.manyDerived[ 0 ].salary = 990
+			testUser.manyDerived[ 1 ].salary = 1990
 
 			await model.save( testUser )
 		})
@@ -176,7 +185,7 @@ describe( 'Model', ()=>{
 		it( 'should save a document as a reference', async ()=>{
 			const subClassModel = Store.getModel( 'SubClass' )
 			expect( subClassModel ).toBeDefined()
-	
+
 			const newDocument = await subClassModel.findById( testUser.documentRef.id ) as SubClass
 
 			expect( newDocument ).toBeInstanceOf( SubClass )
@@ -187,16 +196,15 @@ describe( 'Model', ()=>{
 			const loadedUser = await model.findById( testUser.id )
 
 			expect( loadedUser.documentRef ).toBeInstanceOf( SubClass )
-			expect( loadedUser.documentRef.id ).toEqual( testUser.documentRef.id )
+			expect( loadedUser.documentRef.id ).toBeUndefined()
 			expect( loadedUser.documentRef.year ).toBeUndefined()
-			expect( loadedUser.documentRef.wasLoaded ).toBeFalsy()
 		})
 
 		it( 'should fill data of swallow document reference', async ()=>{
 			const loadedUser = await model.findById( testUser.id )
 
 			await Store.populate( loadedUser.documentRef )
-			expect( loadedUser.documentRef.wasLoaded ).toBeTruthy()
+			expect( loadedUser.documentRef.id ).toBeDefined()
 			expect( loadedUser.documentRef.year ).toBe( 2045 )
 		})
 
@@ -215,21 +223,80 @@ describe( 'Model', ()=>{
 			
 			expect( loadedUser.manyRefs ).toHaveLength( 2 )
 			expect( loadedUser.manyRefs[0] ).toBeInstanceOf( SubClass )
-			expect( loadedUser.manyRefs ).toEqual( expect.arrayContaining([
-				expect.objectContaining({ id: ref1.id }),
-				expect.objectContaining({ id: ref2.id })
-			]))
+			expect( loadedUser.manyRefs[0].id ).toBeUndefined()
 			expect( loadedUser.manyRefs[0].year ).toBeUndefined()
+			expect( loadedUser.manyRefs[1] ).toBeInstanceOf( SubClass )
+			expect( loadedUser.manyRefs[1].id ).toBeUndefined()
+			expect( loadedUser.manyRefs[1].year ).toBeUndefined()
 		})
 
 		it( 'should fill array of refs', async ()=>{
 			const loadedUser = await model.findById( testUser.id )
 			await Store.populate( loadedUser.manyRefs )
 
-			expect( loadedUser.manyRefs[0].year ).toBe( 2081 )
-			expect( loadedUser.manyRefs[1].year ).toBe( 2082 )
+			expect( loadedUser.manyRefs[ 0 ].year ).toBe( 2081 )
+			expect( loadedUser.manyRefs[ 1 ].year ).toBe( 2082 )
 		})
 
+		it( 'should save a reference when declared @persistentAt', async ()=>{
+			const loadedUser = await model.findById( testUser.id )
+
+			expect( loadedUser.derived.id ).toBeUndefined()
+
+			await Store.populate( loadedUser.derived )
+
+			expect( loadedUser.derived.salary ).toBe( 1350 )
+			expect( loadedUser.derived.id ).toBe( testUser.derived.id )
+		})
+
+		it( 'should populate from special collection when declared with @persistentRefAt', async ()=>{
+			const loadedUser = await model.findById( 'user6' )
+			await Store.populate( loadedUser.derived )
+
+			expect( loadedUser.derived.salary ).toBe( 2800 )
+			expect( loadedUser.derived.id ).toBe( 'user4' )
+		})
+		
+		it( 'should save a reference when declared @persistentAt as array', async ()=>{
+			const loadedUser = await model.findById( testUser.id )
+
+			expect( loadedUser.manyDerived[0].id ).toBeUndefined()
+
+			await Store.populate( loadedUser.manyDerived )
+
+			expect( loadedUser.manyDerived[0].salary ).toBe( 990 )
+			expect( loadedUser.manyDerived[0].id ).toBe( testUser.manyDerived[0].id )
+			expect( loadedUser.manyDerived[1].salary ).toBe( 1990 )
+			expect( loadedUser.manyDerived[1].id ).toBe( testUser.manyDerived[1].id )
+		})
+
+		it( 'should not overwrite not filled ref in collection', async ()=>{
+			const loadedUser = await model.findById( 'user6' )
+			await model.save( loadedUser )
+			const refInCollection = await model.findById<DerivedUser>( 'user4' )
+
+			expect( refInCollection.salary ).toBe( 2800 )
+		})
+
+		it( 'should save loaded ref with assigned new instance', async ()=>{
+			const loadedUser = await model.findById( 'user6' )
+			loadedUser.derived = new DerivedUser()
+			loadedUser.derived.salary = 345
+			await model.save( loadedUser )
+
+			const refInCollection = await model.findById<DerivedUser>( loadedUser.derived.id )
+			expect( refInCollection.salary ).toBe( 345 )
+		})
+
+		it( 'should save loaded ref with modified ref data', async ()=>{
+			const loadedUser = await model.findById( 'user6' )
+			await Store.populate( loadedUser.derived )
+			loadedUser.derived.salary = 1623
+			await model.save( loadedUser )
+
+			const refInCollection = await model.findById<DerivedUser>( 'user4' )
+			expect( refInCollection.salary ).toBe( 1623 )
+		})
 	})
 
 	describe( 'Operations on queries', ()=>{
@@ -244,30 +311,30 @@ describe( 'Model', ()=>{
 		it( 'should sort ascending the result set', async ()=>{
 			const docs = await model.find().orderBy( 'age' ).get()
 
-			expect( docs[0].id ).toEqual( 'user2' )
-			expect( docs[1].id ).toEqual( 'user1' )
+			expect( docs[ 0 ].id ).toEqual( 'user2' )
+			expect( docs[ 1 ].id ).toEqual( 'user1' )
 		})
-		
+
 		it( 'should sort descending the result set', async ()=>{
 			const docs = await model.find().orderBy( 'age', 'desc' ).get()
 
-			expect( docs[0].id ).toEqual( 'user3' )
-			expect( docs[1].id ).toEqual( 'user4' )
+			expect( docs[ 0 ].id ).toEqual( 'user3' )
+			expect( docs[ 1 ].id ).toEqual( 'user5' )
 		})
 
 		it( 'should sort by deep property path', async ()=>{
 			const docs = await model.find().orderByDeepProp( 'name.firstName', 'desc' ).get()
 
-			expect( docs[0].id ).toEqual( 'user4' )
-			expect( docs[1].id ).toEqual( 'user3' )
+			expect( docs[ 0 ].id ).toEqual( 'user6' )
+			expect( docs[ 1 ].id ).toEqual( 'user5' )
 		})
-		
+
 		it( 'should sort by swallow property path', async ()=>{
 			const docs = await model.find().orderByDeepProp( 'age' ).get()
 
-			expect( docs[0].id ).toEqual( 'user2' )
-			expect( docs[1].id ).toEqual( 'user1' )
-		})		
+			expect( docs[ 0 ].id ).toEqual( 'user2' )
+			expect( docs[ 1 ].id ).toEqual( 'user1' )
+		})
 
 		xdescribe( 'Data Cursors', ()=>{
 			beforeEach( async ()=>{
@@ -277,29 +344,29 @@ describe( 'Model', ()=>{
 			it( 'should get next result set', async ()=>{
 				const docs = await model.next()
 				expect( docs ).toHaveLength( 2 )
-				expect( docs[0].id ).toEqual( 'user3' )
+				expect( docs[ 0 ].id ).toEqual( 'user3' )
 			})
-			
+
 			it( 'should get previous result set', async ()=>{
 				await model.next()
 				await model.next()
 				const docs = await model.prev()
 				expect( docs ).toHaveLength( 2 )
-				expect( docs[0].id ).toEqual( 'user3' )
+				expect( docs[ 0 ].id ).toEqual( 'user3' )
 			})
 
 			it( 'should not go lower than begining of result set', async ()=>{
 				const docs = await model.prev()
 				expect( docs ).toHaveLength( 0 )
 			})
-			
+
 			it( 'should not go beyond the end of result set', async ()=>{
 				await model.next()
 				await model.next()
 				const docs = await model.next()
 				expect( docs ).toHaveLength( 0 )
 			})
-			
+
 		})
 	})
 
