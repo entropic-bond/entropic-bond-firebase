af249268f807d68b832169fbf5f6175c4faac502
jseto <jsetojseto@gmail.com>
2023-10-29 03:23:57 +0700

    run tests on node env

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
 		globals: true,
+		environment: 'node',
 		exclude: ['**/node_modules', '**/dist', '.idea', '.git', '.cache','**/lib', '**/out'],
 	},
 })
